34d434d582d7ddd2c01dd394601db5dfb46c07b3
sergestux <sergestux@users.noreply.github.com>
2021-04-01 07:54:18 -0600

    Update index.php
    Al final logré conseguir el ejemplo con php y javascript. Si se desea hacer con html puro, habría que escribir manualmente el código html de los 10 timers utilizados en el ejemplo, que prácticamente fue para lo único que utilicé PHP

--- index.php
+++ index.php
@@ -7,66 +7,100 @@
 
 <form>
 <?php
-  for($Cont=1; $Cont<=5;$Cont++)
-    echo "
-        <label for='Nombre-$Cont'>Reloj $Cont:</label>
-        <input type='text' id='txtReloj-$Cont' name='Nombre-$Cont' class='btnReloj' Clave='$Cont'>
-        <input id='reloj-$Cont' style='width: 180px; background-color: black;font-size:50px;color: green; text-align: center' value='0:00'>
-        <br>";
-  ?>
+$TAM=10;
+
+for($Cont=1; $Cont<=$TAM;$Cont++)
+  echo "
+      <label for='Nombre-$Cont'>Reloj $Cont:</label>
+      <input type='text' id='txtReloj-$Cont' name='Nombre-$Cont' class='btnReloj' Clave='$Cont'>
+      <input id='reloj-$Cont' style='width: 100px; background-color: black;font-size:40px;color: green; text-align: center' value='00:00'>
+      <br>";
+?>
 </form>
 
 </body>
 
 <script>
-const MINUTO = 1000;
-var FechaInicial;
-var FechaFinal;
-var Cont;
 
-$(document).on("keypress", ".btnReloj", function(e) {
+  const MINUTO = 1000;
+  const MILLISECONDS_OF_A_MINUTE = MINUTO * 60;
+  const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
 
-  Cont = $(this).attr('Clave');
-  var Tiempo = $(this).val();
+  //var TAM=>   //TRAIGO LA VARIABLE DE PHP
+  const TAM=<?=$TAM;?>;
+  
+  var Cronometros= new Array();
+  Cronometros['FechaIni'] = new Array(TAM);   //Fecha inicial
+  Cronometros['FechaFin']= new Array(TAM);    //Fecha Final
+  Cronometros['Activo']= new Array(TAM);      //Si debo seguir restando el tiempo o ya terminó
+  Cronometros['Division']= new Array(TAM);     //Para partir el tiempo en intervalos y se pueda cambiar de color
+ 
+  setInterval(actualizarCronometros, MINUTO); // Refresh every second
 
-  var code = (e.keyCode ? e.keyCode : e.which);
-  if (code == 13 && Tiempo > 0) {
-    console.log ('Iniciar Reloj ' + Cont + ' por ' + Tiempo +  ' minutos');
+  $(document).on("keypress", ".btnReloj", function(e) {
 
-    //OBTENEMOS LA FECHA ACTUAL
-    FechaInicial = new Date(Date.now());
-    FechaFinal = new Date(Date.now());
-    //LE SUMO LOS MINUTOS A LA FECHA FINAL  
-    FechaFinal.setMinutes(FechaInicial.getMinutes() + parseInt( Tiempo)); //le sumo los minutos a la fecha de creacion
-    //IMPRIMO LAS FECHAS INICIALES Y FINALES
-    console.log('Fecha Inicial:' + FechaInicial);
-    console.log('Fecha Final: ' + FechaFinal);
+    Cont = $(this).attr('Clave');
+    var Tiempo = $(this).val();
 
-    actualizarCronometro();
-    setInterval(actualizarCronometro, MINUTO ); // Refresh every second
-          
-  } //Fin del Si se presiona Enter
+    var code = (e.keyCode ? e.keyCode : e.which);
+    if (code == 13 && Tiempo > 0) {
+      console.log ('Iniciar Reloj ' + Cont + ' por ' + Tiempo +  ' minutos');
+
+      //OBTENEMOS LA FECHA ACTUAL
+      FechaInicial = new Date(Date.now());
+      FechaFinal = new Date(Date.now());
+      //LE SUMO LOS MINUTOS A LA FECHA FINAL  
+      FechaFinal.setMinutes(FechaInicial.getMinutes() + parseInt( Tiempo)); //le sumo los minutos a la fecha de creacion
+      //IMPRIMO LAS FECHAS INICIALES Y FINALES
+      console.log('Fecha Inicial:' + FechaInicial);
+      console.log('Fecha Final: ' + FechaFinal);
+      Cronometros['FechaIni'][Cont]=FechaInicial;  
+      Cronometros['FechaFin'][Cont]=FechaFinal;     
+      Cronometros['Activo'][Cont]=true; //Activo el cronometro
+      Cronometros['Division'][Cont]=parseInt((FechaFinal-FechaInicial)/5);
+
+    } //Fin del Si se presiona Enter
 })
 
-const MILLISECONDS_OF_A_MINUTE = MINUTO * 60;
-const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+function actualizarCronometros(Tiempo) {
 
-function actualizarCronometro(Tiempo) {
-    const NOW = new Date();
-    const DURATION = FechaFinal - NOW;
-    const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
-    const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MINUTO);
-    // Thanks Pablo Monteserín (https://pablomonteserin.com/cuenta-regresiva/)
+  //Recorro todos los timers
+  for(Cont=1; Cont<=TAM; Cont++)
+  {    
+    if(Cronometros['Activo'][Cont])
+    {
+      var DURATION = Cronometros['FechaFin'][Cont] - Cronometros['FechaIni'][Cont];   //Duracion en segundos
+      var REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
+      var REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MINUTO);
+    
+      
+      var TiempoRestante = REMAINING_MINUTES.toString().padStart(2,'0') + ':' + REMAINING_SECONDS.toString().padStart(2,'0');
+      
+      $("#reloj-" + Cont).val(TiempoRestante);
+      Cronometros['FechaFin'][Cont].setSeconds(Cronometros['FechaFin'][Cont].getSeconds()-1);  //Le resto un segundo a la fecha final
 
-    var TiempoRestante = REMAINING_MINUTES + ':' + REMAINING_SECONDS;
-    console.log(Cont + ': ' + TiempoRestante);
-    $("#reloj-" + Cont).val(TiempoRestante);
-  
-    if (TiempoRestante == '0:0') {
-        console.log("Tiempo Terminado");
-        //COMO DIABLOS LO PARO :-(
-        return;
-    }
+      if (TiempoRestante == '00:00') {
+        console.log("Tiempo Terminado, reloj: " + Cont);        
+        Cronometros['Activo'][Cont]=false;
+        
+      }
+
+      //DECIDO QUE COLOR DEBE TENER EL CRONOMETRO
+      Division=Cronometros['Division'][Cont];
+      console.log('Cont ' + Cont + ': ' + TiempoRestante);  
+      if (DURATION> (Division*2))
+        console.log('VERDE');
+      else
+      {
+        if (DURATION> Division)
+          console.log('NARANJA');
+        else
+          console.log('ROJO');
+      }
+    }  
+
+  }  
+
 }
 
 </script>
